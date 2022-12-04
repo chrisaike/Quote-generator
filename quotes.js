7062f0ecd66fe0f4538def5002ab627855fc032e
chris <chrisaike1@gmail.com>
2022-12-04 14:44:48 -0500

    first commit

---
+++ quotes.js
@@ -0,0 +1,43 @@
+const localQuotes = [
+    {
+        text: 'The best way to begin a new project is begin',
+        author: 'CA',
+    },
+    {
+        text: 'You will never run out of steam for new challenges if you know your source of sustenance',
+        author: 'CA',
+    },
+    {
+        text: 'Give me health, and I will show you the value of live',
+        author: 'CA',
+    },
+    {
+        text: 'Tell me whom you move with and I will tell you what you are',
+        author: 'My Dad',
+    },
+    {
+        text: 'Nne ebule ako si umu ya: Ihe o ga emegi ugboro ole ma imara ihe?',
+        author: 'Igbo Proverb',
+    },
+    {
+        text: 'With patience and consistency, you can learn anything.',
+        author: 'CA',
+    },
+    {
+        text: 'Consistency is the roadmap to mastery.',
+        author: 'CA',
+    },
+    {
+        text: 'One can gain a mastery by will or default as long as the element of consistency resides somewhere in the equation.',
+        author: 'CA',
+    },
+    {
+        text: 'To walk with God, learn the art of Praising Him.',
+        author: 'CA via Bible insights',
+    },
+    { 
+        text: 'When you praise, God apraises you!',
+        author: 'CA via Bible insights',
+
+    }
+]
